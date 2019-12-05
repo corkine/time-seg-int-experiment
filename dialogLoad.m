9ad7970f7419ed47ce96371a7b80c2c85b1188eb
corkine <corkine@outlook.com>
2019-12-05 11:49:26 +0800

    fix dialogLoad

--- dialogLoad.m
+++ dialogLoad.m
@@ -1,5 +1,6 @@
 function CONF = dialogLoad(CONF)
-%DIALOGLOAD 从对话框中加载配置，结果写入 CONF struct 中
+%DIALOGLOAD 从对话框中加载配置，结果写入 CONF struct 中。
+%CONF 可不提供，如果提供，务必包含 debug 字段，布尔值。
 %   如果是 debug 模式，则自动复写默认值，反之，如果取消，则返回其本身（如果没有参数。则返回空 struct），
 %   如果确定则返回对话框中值（包括默认值）。
 
